b068cb787fa8831702eee9b269ff3d0c6428debc
Marlon Andrei <marlon@micrologi.com.br>
2022-04-09 10:01:27 -0300

    Commit do sistema
    Commit do sistema

---
+++ src/app/painel-vagas/painel-vagas.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
+
+@Component({
+  selector: 'app-painel-vagas',
+  templateUrl: './painel-vagas.component.html',
+  styleUrls: ['./painel-vagas.component.css']
+})
+
+export class PainelVagasComponent implements OnInit {
+
+  public vaga: Vaga = new Vaga(0,"","","",0);
+  public vagas: Vaga[] = [];
+
+  constructor(private _vagasService: VagasService) { }
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(){
+    this._vagasService.cadastrarVaga(this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("Falha ao cadastrar vaga!")}
+    );
+    window.location.href = "/mural";
+  }
+
+  atualizar(id: number){
+    this._vagasService.atualizarVaga(id,this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("Falha ao atualizar vaga!")}
+    );
+    window.location.href = "/mural";
+
+  }
+
+  excluir(id: number){
+    this._vagasService.removerVaga(id).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("Falha ao excluir vaga!")}
+    );
+    window.location.href = "/mural";
+  }
+  
+}
